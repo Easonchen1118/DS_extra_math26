280afba615b11e4937a3ef387b278e0ee3e9fc4b
Easonchen1118 <120409122+Easonchen1118@users.noreply.github.com>
2022-12-27 18:13:04 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//輸出 1*1、2*2、...、N*N之結果
+//輸出 1*1、2*2、...、N*N之結果https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?id=6928
 int main(){
     int num;
     scanf("%d", &num);
